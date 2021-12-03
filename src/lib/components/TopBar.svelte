b550969793ad89835dae43ffeef385958441c0a8
Jithu S Nair <work.jithus@gmail.com>
2021-12-03 21:30:34 +0530

    style: Change logout button to a dropdown menu which includes profile-settings link and logout option

--- src/lib/components/TopBar.svelte
+++ src/lib/components/TopBar.svelte
@@ -1,17 +1,23 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
+	import { page } from '$app/stores';
 	import { signOut, getAuth } from '@firebase/auth';
 	import { app } from '$lib/configs/firebase';
 
 	import TopAppBar, { Row, Section, Title } from '@smui/top-app-bar';
 	import IconButton from '@smui/icon-button';
+	import MenuSurface, { MenuSurfaceComponentDev } from '@smui/menu-surface';
+	import List, { Item, Separator, Text, Meta } from '@smui/list';
 
 	import { leftDrawerOpen, smallScreen } from '$lib/store/drawers';
 	import { darkMode } from '$lib/store/settings';
+	import { goto } from '$app/navigation';
 
 	export let hideTopBar = false;
 
 	let showDrawerButton = false;
+
+	let menu: MenuSurfaceComponentDev;
 
 	onMount(() => {
 		let darkModeStored = window.localStorage.getItem('darkMode');
@@ -48,16 +54,21 @@
 	function toggleDrawer() {
 		$leftDrawerOpen = !$leftDrawerOpen;
 	}
+
+	function profileEdit() {
+		goto('/dashboard/profile');
+		menu.setOpen(false);
+	}
 </script>
 
 {#if !hideTopBar}
 	<TopAppBar variant="static">
 		<Row>
 			<Section>
-				{#if showDrawerButton}
+				{#if showDrawerButton && $page.path !== '/dashboard/profile'}
 					<IconButton on:click={toggleDrawer} class="material-icons">menu</IconButton>
 				{/if}
-				<Title>TASKR</Title>
+				<Title on:click={() => goto('/dashboard')} style="cursor: pointer">TASKR</Title>
 			</Section>
 			<Section align="end" toolbar>
 				<IconButton
@@ -71,8 +82,25 @@
 						dark_mode
 					{/if}
 				</IconButton>
-				<IconButton on:click={logout} class="material-icons" aria-label="Account menu button">
-					logout
+				<div>
+					<MenuSurface bind:this={menu}>
+						<List>
+							<Item on:click={profileEdit}>
+								<Text>Profile Settings</Text>
+							</Item>
+							<Separator />
+							<Item on:click={logout}>
+								<Text>Log out</Text>
+							</Item>
+						</List>
+					</MenuSurface>
+				</div>
+				<IconButton
+					on:click={() => menu.setOpen(true)}
+					class="material-icons"
+					aria-label="Account menu button"
+				>
+					person
 				</IconButton>
 			</Section>
 		</Row>
